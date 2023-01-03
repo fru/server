c20adf34db2cc1f390c3d72bd8b201a95a515cfa
Florian Rüberg <florian.rueberg@gmail.com>
2023-01-03 19:16:20 +0100

    Update ecosystem.config.js

--- hooks/ecosystem.config.js
+++ hooks/ecosystem.config.js
@@ -1,1 +1,11 @@
-
+module.exports = {
+  apps : [{
+    name   : "hooks",
+    script : "./server.js",
+    "watch": true,
+    "watch_options": {
+      "followSymlinks": false,
+      "usePolling": true
+    }
+  }]
+}
