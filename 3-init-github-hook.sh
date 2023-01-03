d81e21e50ce3991dfcbd550fbb1dccfe498ff3c8
Florian Rüberg <florian.rueberg@gmail.com>
2023-01-03 19:31:51 +0100

    Update 3-init-github-hook.sh

--- 3-init-github-hook.sh
+++ 3-init-github-hook.sh
@@ -3,7 +3,9 @@ apt-get install nodejs npm -y
 npm install ./hooks
 npm install -g pm2
 
-pm2 start ./hooks/ecosystem.config.js
+cd ./hooks
+pm2 start ./ecosystem.config.js
 pm2 save
+cd ..
 
 pm2 startup -u root
