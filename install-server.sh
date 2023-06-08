e26e65a6bdc7b40d807c78519fd1fead7c09e579
Florian Rüberg <florian.rueberg@gmail.com>
2023-06-08 02:51:29 +0200

    Update install-server.sh

--- install-server.sh
+++ install-server.sh
@@ -1,1 +1,8 @@
+#!/bin/bash
 
+git clone https://github.com/fru/server.git ~/.install
+cp ~/.install/secrets.env /data/secrets.env
+mkdir --parents /opt/swag/nginx/site-confs
+echo "include /install/nginx.conf;" >| /opt/swag/nginx/site-confs/default.conf
+
+docker-compose -f ~/.install/docker-compose.yml up -d --build
