94a90154fc4147f44c1ba46d7dbd74dd4a516a35
Florian Rüberg <florian.rueberg@gmail.com>
2023-06-08 02:54:27 +0200

    Update install-btld-web.sh

--- install-btld-web.sh
+++ install-btld-web.sh
@@ -1,1 +1,9 @@
+#!/bin/bash
 
+git clone https://github.com/fru/btld-web.git ~/btld-web
+
+export $(grep -v '^#' /data/secrets.env | xargs -d '\n') 
+export COMPOSE_PROJECT_NAME=btld-web
+docker-compose -f ~/btld-web/@btld-web/server/docker-compose.yml up -d --build
+
+chown -R 5050:5050 /data/pg_admin
